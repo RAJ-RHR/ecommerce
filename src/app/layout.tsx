7a19f655e6ff0d919a46d19e6ba8457522fffe4a
Raj Hansh Raj <rajhansraj2000@gmail.com>
2025-07-14 15:30:32 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,17 @@ import ClientWrapper from './ClientWrapper';
 export const metadata = {
   title: 'Herbolife Store',
   description: 'Buy Herbal Products Online',
+  icons: {
+    icon: '/logos.png',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/logos.png" />
+      </head>
       <body>
         <ClientWrapper>{children}</ClientWrapper>
       </body>
