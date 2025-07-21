f1fe47bdae3b2a2fa4eca3ca9bec8d15a216b8d1
RAJ-RHR <rajhansraj2000@gmail.com>
2025-07-21 15:47:22 +0530

    analy

--- next.config.js
+++ next.config.js
@@ -8,6 +8,23 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all routes
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://herbolife.in', // Allow embedding in herbolife.in
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors https://herbolife.in;", // CSP support
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
